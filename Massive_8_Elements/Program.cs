0eee1d41042e771a3c2ef4921b92a85f889a3fc8
Mike-P63 <rumaish@rambler.ru>
2023-02-18 13:13:13 +0400

    Added massive 8 Elements

--- Massive_8_Elements/Program.cs
+++ Massive_8_Elements/Program.cs
@@ -20,7 +20,9 @@ void Array()
     for (int i = 0; i < Array.Length; i++)
     {
         Array[i] = new Random().Next(0, 2);
-        Console.Write(Array[i] + " ");
+        Console.Write(Array[i] + ", ");
     }
 }
+Console.Write($"[ ");
 Array();
+Console.Write($"]");
