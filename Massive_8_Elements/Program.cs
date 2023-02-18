fd16fdfc62bc6e7b818b5ab37c058c62e78952da
Mike-P63 <rumaish@rambler.ru>
2023-02-18 13:06:05 +0400

    Added massive 8 Elements

---
+++ Massive_8_Elements/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 30: Напишите программу, которая выводит массив 
+из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/ 
+
+Console.Clear();
+
+int Massive(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int ArrayLen = Massive("Введите длинну массива: ");
+
+void Array()
+{
+    int[] Array = new int[ArrayLen];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(0, 2);
+        Console.Write(Array[i] + " ");
+    }
+}
+Array();
