6ed011a3ddc2139b87861e5e9f03eaacfb872711
Mike-P63 <rumaish@rambler.ru>
2023-02-18 12:34:47 +0400

    Added task 28 Sum of digits

---
+++ Task_28_Sum_Digits/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 28: Напишите программу, которая принимает на вход 
+число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+int sumDigits(int start, int end)
+{
+    int sum = 1;
+    for (int i = 1; i <= end; i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+
+
+int number = getUserData("Введите число");
+int result = sumDigits(1, number);
+
+Console.WriteLine($"Произведение чисел от 1 до {number} = {result}");
+
+
