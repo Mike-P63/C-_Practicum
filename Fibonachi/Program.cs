5a6b2694e998c06efaf4130f9e5d914526445300
Mike-P63 <rumaish@rambler.ru>
2023-02-21 18:06:02 +0400

    Added Task 44 Fibonachi

---
+++ Fibonachi/Program.cs
@@ -0,0 +1,34 @@
+﻿
+/*Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи.
+ Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+*/
+
+Console.Clear();
+
+int getUserData(string message)
+{
+    Console.WriteLine(message);
+    int userData = int.Parse(Console.ReadLine()!);
+    return userData;
+}
+
+string Fibonachi(int number)
+{
+    int n1 = 0;
+    int n2 = 1;
+    string result = $"{n1}{n2}";
+    for (int i = 0; i < number - 2; i++)
+    {
+        int n = n1 + n2;
+        result = result + n;
+        n1 = n2;
+        n2 = n;
+    }
+    return result;
+}
+int number = getUserData("Введите число:");
+string result = Fibonachi(number);
+Console.WriteLine(result);
