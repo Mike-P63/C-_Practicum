d5c62d86368b553815e3d44c259c2ac0d3d36a72
Mike-P63 <rumaish@rambler.ru>
2023-02-26 13:22:28 +0400

    Added Task 38 Diffference MAx Min

---
+++ Task_38_Dif_Max_Min/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+Console.Clear();
+
+int size = 8;
+int[] numbers = new int[size];
+
+void FillArrayRandomNumbers(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 300);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine(" ]");
+}
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+int max = numbers[0];
+int min = numbers[0];
+int dif = 0;
+void Difference(int max, int min, int dif)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > max)
+        {
+        max = numbers[i];
+        }
+        else if (numbers[i] < min)
+        {
+        min = numbers[i];
+        }
+        dif = max - min;
+    }
+Console.WriteLine($"Минимальное число: {min}");
+Console.WriteLine($"Максимальное число: {max}");
+Console.WriteLine($"Разница между максимальным и минимальным числами: {dif}");
+}
+Difference(max,min,dif);
+
